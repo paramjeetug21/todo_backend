2cd2ccfacd0a7df23cad3dc8b367d15346b2322f
paramjeetug21 <paramjeet.ug21@nsut.ac.in>
2025-11-13 13:34:51 +0530

    build for vercel deployment

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+let cachedServer: any;
 
-  app.enableCors({
-    origin: 'https://new-nine-silk-25.vercel.app', // your frontend URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true, // if you want to send cookies/auth headers
-  });
-
-  await app.listen(process.env.PORT ?? 3000);
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  if (!cachedServer) {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+      origin: 'https://new-nine-silk-25.vercel.app',
+      methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+      credentials: true,
+    });
+    await app.init();
+    cachedServer = app.getHttpAdapter().getInstance();
+  }
+  return cachedServer(req, res);
 }
-bootstrap();

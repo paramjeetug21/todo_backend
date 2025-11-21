3ef43caf9e7823f8afbfcee084b3df1b94fd1984
paramjeetug21 <paramjeet.ug21@nsut.ac.in>
2025-11-21 21:24:58 +0530

    fix: install pg package

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -1,30 +1,36 @@
 import { Sequelize } from 'sequelize-typescript';
 import { User } from '../user/user.entity';
 import { Todos } from '../task/task.entity';
+import pg from 'pg';
+
+const sequelize = new Sequelize({
+  dialect: 'postgres',
+  dialectModule: pg,
+  host: 'aws-1-ap-southeast-1.pooler.supabase.com',
+  port: 5432,
+  username: 'postgres.lyoebknxanxqshedtwpr',
+  password: 'Abcd@1234',
+  database: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
+  },
+});
+
+sequelize.addModels([User, Todos]);
 
 export const databaseProviders = [
   {
     provide: 'SEQUELIZE',
     useFactory: async () => {
-      const sequelize = new Sequelize({
-        dialect: 'postgres',
-        host: 'aws-1-ap-southeast-2.pooler.supabase.com',
-        port: 5432,
-        username: 'postgres.ajzbtmmzjujadamdrvbj',
-        password: 'Abcd@1234',
-        database: 'postgres',
-        dialectOptions: {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-        },
-      });
-
-      sequelize.addModels([User, Todos]);
-
       await sequelize.sync();
-
       return sequelize;
     },
   },

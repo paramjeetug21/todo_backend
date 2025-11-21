91e8b328980f23e9f793e027bfba9725bbc70ba4
paramjeetug21 <paramjeet.ug21@nsut.ac.in>
2025-11-21 21:27:40 +0530

    fix: install pg package

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { TaskModule } from './task/task.module';
 import { DatabaseModule } from './database/database.module';
+import { Basic } from './src.controller';
 
 @Module({
+  controllers: [Basic],
   imports: [DatabaseModule, UserModule, TaskModule],
 })
 export class AppModule {}
